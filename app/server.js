f3c40b0e67b88895723cd739c9006857713d4c84
zzy1993 <zzy1993@163.com>
2017-07-21 15:26:27 -0400

    0721 Set file and api

--- app/server.js
+++ app/server.js
@@ -1,7 +1,16 @@
 var http = require("http");
+var fs =  require("fs");
 
 http.createServer(function (req, res){
-	res.writeHead(200, {"Content-Type":"text-plain"});
-	res.write("Hello world!");
-	res.end();
+	if(req.url === "/"){
+		res.writeHead(200, {"Content-Type": "text-html"});
+		var html = fs.readFileSync(__dirname + "/index.html");
+		res.write(html);
+		res.end();
+	}
+	if(req.url === "/api"){
+		res.writeHead(200, {"Content-Type": "application/json"});
+		var obj = {"content": "Welcome"};
+		res.write(JSON.stringify(obj));
+	}
 }).listen(3000);
